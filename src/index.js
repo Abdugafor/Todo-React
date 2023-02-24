3fe8dc8bff89aeaa592ae14beb5d1a5582eb2b0d
Abdugafor Gafarov <abdugaforgafarov0111@gmail.com>
2023-02-24 15:00:10 +0500

    nnn

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+class WhoAmI extends Component {
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     year: 26
+  //   }
+  //   this.nextYear = this.nextYear.bind(this)
+  // }
+  state = {
+    year: 26
+  }
+  nextYear = () => {
+    this.setState(state => ({
+      year: ++state.year
+    }))
+  }
+
+  render() {
+    const {name, surname , link} = this.props
+    const {year} = this.state
+    return (
+      <>
+          <button onClick={this.nextYear}>++</button>
+          <h1>My name is {name} , surname - {surname}, year = {year} </h1>
+          <a href={link}>MyProfile</a>
+      </>
+    )
+  }
+}
+
+
+const All = () => {
+  return (
+    <>
+      <WhoAmI name="Abdugafor" surname="Gafarov" link="facebook.com"/>
+      <WhoAmI name="Andrey" surname="Lirov" link="facebook.com"/>
+      <WhoAmI name="Good" surname="Makov" link="facebook.com"/>
+    </>
+  )
+}
 root.render(
   <React.StrictMode>
-    <App />
+    <All/>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
