80155e2fe392ad74577c15a374cd44da67b19fa6
Abdugafor Desktop <gafarovabdugafur@gmail.com>
2023-05-08 21:17:09 +0500

    dfsaf

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-
+import './global.css';
+import App from './Components/app';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
