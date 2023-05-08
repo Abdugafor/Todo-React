7f3ed56bd4d524708c334f71904f5a17998c0954
Abdugafor Desktop <gafarovabdugafur@gmail.com>
2023-05-08 21:14:06 +0500

    fdf

--- src/index.js
+++ src/index.js
@@ -1,46 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-// import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './global.css';
+import App from './Components/app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-
-const Header = () => {
-      return <h2>Wassup Dude</h2>
-}
-
-const Field = () => {
-  const styleField = {
-    width: '300px'
-  }
-  return <input
-               type="text" 
-               style={styleField}
-               />
-}
-
-const Btn = () => {
-  const text = "Log in"
-  const logged = false
-  return <button>{logged ? null : text}</button>
-
-}
-
-const App = () => {
-  return (
-    <div>
-      <Header/>
-      <Field/>
-      <Btn/>
-
-    </div>
-  )
-}
-
 root.render(
-  <App/>
+    <App/>
 );
 
 
