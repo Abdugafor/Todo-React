702800f07364fe3ec3e6213bd3c7b60f81c45bce
Abdugafor Desktop <gafarovabdugafur@gmail.com>
2023-05-16 22:14:26 +0500

    little changes

--- src/Components/todosList/todoItem/todoItem.js
+++ src/Components/todosList/todoItem/todoItem.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import './todoItem.css'
 
 export function TodoItem({todo, addClassDoneFunc}) {
+    const [checked, setChecked] = useState(false)
+
+    const handleClick = () => {
+        setChecked(!checked)
+    }
     return (
         <li className="listItem" onClick={addClassDoneFunc}>
-                <input type="checkbox" id={todo.id}/>
+                <input type="checkbox" id={todo.id} checked={checked} onClick={handleClick}/>
                 <label for={todo.id} key={todo.id} className={todo.done ? "done" : ''}>{todo.title}</label>
         </li>
     )
